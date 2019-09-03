268df71e5276df157e1f026c0decd51bcd68adf6
Matheus Barbosa <matheusb565@gmail.com>
2019-09-03 14:38:45 -0300

    Criando um interceptor para tratamento de erros

--- src/services/domain/categoria.service.ts
+++ src/services/domain/categoria.service.ts
@@ -1,4 +1,4 @@
-import { CategotiaDTO } from './../../models/categoria.dto';
+import { CategoriaDTO } from './../../models/categoria.dto';
 import { API_CONFIG } from './../../config/api.config';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
@@ -9,8 +9,8 @@ export class CategoriaService{
 
   constructor(public http: HttpClient){}
 
-  findAll() : Observable<CategotiaDTO[]> {
-    return this.http.get<CategotiaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
+  findAll() : Observable<CategoriaDTO[]> {
+    return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
   }
 
 }
