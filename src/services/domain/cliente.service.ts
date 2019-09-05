6f66cd6b139845e63b2d9690cece249e0ca237c1
Matheus Barbosa <matheusb565@gmail.com>
2019-09-05 11:42:12 -0300

    Povoando dinamicamente estado e cidade

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,7 +1,7 @@
 import { API_CONFIG } from './../../config/api.config';
 import { ClienteDTO } from './../../models/cliente.dto';
 import { StorageService } from './../storage.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs/Rx';
 
